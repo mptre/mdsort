115918aa38a071de5b2f31a1805b81bce4caccc4
Anton Lindqvist <anton.lindqvist@gmail.com>
2018-03-29 20:00:50 +0200

    Avoid falling back to rand(3) on platforms missing arc4random(3)
    CID: 266912

--- compat-arc4random.c
+++ compat-arc4random.c
@@ -4,13 +4,42 @@ int unused;
 
 #ifndef HAVE_ARC4RANDOM
 
+#include <fcntl.h>
 #include <stdint.h>
-#include <stdlib.h>
+#include <unistd.h>
+
+static int rrand(const char *, uint32_t *);
 
 uint32_t
 arc4random(void)
 {
-	return rand();
+	uint32_t r;
+
+	/*
+	 * The random number is only used to construct unique filenames.
+	 * Since a retry mechnanism is already in place in the case of
+	 * duplicates, nothing fancy is needed.
+	 */
+	if (rrand("/dev/urandom", &r) || rrand("/dev/urandom", &r))
+		return 1; /* fair dice roll */
+	return r;
+}
+
+static int
+rrand(const char *path, uint32_t *r)
+{
+	ssize_t n;
+	int fd, len;
+
+	fd = open(path, O_RDONLY | O_CLOEXEC);
+	if (fd == -1)
+		return 1;
+	len = sizeof(*r);
+	n = read(fd, r, len);
+	close(fd);
+	if (n == -1 || n != len)
+		return 1;
+	return 0;
 }
 
 #endif /* !HAVE_ARC4RANDOM */
