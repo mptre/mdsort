efe6daea81e604ad27259b305f9edb52feda7da6
Anton Lindqvist <anton.lindqvist@gmail.com>
2018-03-30 21:30:55 +0200

    Fix compat-utimensat

--- compat-utimensat.c
+++ compat-utimensat.c
@@ -4,35 +4,53 @@ int unused;
 
 #ifndef HAVE_UTIMENSAT
 
+#include <sys/stat.h>
 #include <sys/time.h>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int
 utimensat(int fd, const char *path __attribute__((__unused__)),
     const struct timespec times[2], int flags __attribute__((__unused__)))
 {
+	struct stat st;
 	struct timeval tv[2];
-	struct timeval now;
+	const struct timespec *atim, *mtim;
+	int errno_save, res, rfd;
 
-	/* XXX not how utimensat() should behave */
-	if (times[0].tv_nsec == UTIME_OMIT) {
-		if (gettimeofday(&now, NULL) == -1)
-			return -1;
-		tv[0] = now;
-	} else {
-		tv[0].tv_sec = times[0].tv_sec;
-		tv[0].tv_usec = times[0].tv_nsec / 1000;
+	/* Assume path is relative to fd. */
+	rfd = openat(fd, path, O_RDONLY | O_CLOEXEC);
+	if (rfd == -1)
+		return -1;
+
+	if ((times[0].tv_nsec == UTIME_OMIT || times[1].tv_nsec == UTIME_OMIT)
+	    && fstat(rfd, &st) == -1) {
+		res = -1;
+		goto ret;
 	}
 
-	if (times[1].tv_nsec == UTIME_OMIT) {
-		if (gettimeofday(&now, NULL) == -1)
-			return -1;
-		tv[1] = now;
-	} else {
-		tv[1].tv_sec = times[1].tv_sec;
-		tv[1].tv_usec = times[1].tv_nsec / 1000;
-	}
+	if (times[0].tv_nsec == UTIME_OMIT)
+		atim = &st.st_atim;
+	else
+		atim = &times[0];
+	tv[0].tv_sec = atim->tv_sec;
+	tv[0].tv_usec = atim->tv_nsec / 1000;
 
-	return futimes(fd, tv);
+	if (times[1].tv_nsec == UTIME_OMIT)
+		mtim = &st.st_mtim;
+	else
+		mtim = &times[1];
+	tv[1].tv_sec = mtim->tv_sec;
+	tv[1].tv_usec = mtim->tv_nsec / 1000;
+
+	res = futimes(rfd, tv);
+ret:
+	errno_save = errno;
+	close(rfd);
+	errno = errno_save;
+	return res;
 }
 
 #endif /* !UTIMENSAT */
