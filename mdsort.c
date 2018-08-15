mptre/mdsort

commit abc73d844f81d771a04a2cfd266b32d3afdd3263

Reduce usage of globals by introducing struct environment

mdsort.c
@@ -10,14 +10,11 @@
 
 #include "extern.h"
 
-const char *home = NULL;
-const char *hostname = NULL;
 int verbose = 0;
 
-static void readenv(void);
+static const char *defaultconf(const struct environment *);
+static void readenv(struct environment *);
 static __dead void usage(void);
-
-static const char *confpath;
 
 int
 main(int argc, char *argv[])
@@ -25,9 +22,10 @@ main(int argc, char *argv[])
 	char path[PATH_MAX];
 	struct config_list *config;
 	struct config *conf;
+	struct environment env;
 	struct maildir *dst, *md;
 	struct message *msg;
-	const char *dstpath;
+	const char *confpath, *dstpath;
 	int c;
 	int dflag = 0;
 	int mdflags = MAILDIR_CREATE;
@@ -64,12 +62,14 @@ main(int argc, char *argv[])
 		verbose = 1;
 
 	/* Extract mandatory data from the current environment. */
-	readenv();
+	readenv(&env);
 
 	if (pledge("stdio rpath wpath cpath fattr", NULL) == -1)
 		err(1, "pledge");
 
-	config = parse_config(confpath);
+	if (confpath == NULL)
+		confpath = defaultconf(&env);
+	config = parse_config(confpath, &env);
 	if (config == NULL)
 		return 1;
 	if (nflag)
@@ -96,7 +96,7 @@ main(int argc, char *argv[])
 			if (dflag)
 				rule_inspect(conf->rule, stdout);
 			else
-				maildir_move(md, dst, msg);
+				maildir_move(md, dst, msg, &env);
 			maildir_close(dst);
 
 next:
@@ -122,39 +122,40 @@ usage(void)
 	exit(1);
 }
 
-static void
-readenv(void)
+static const char *
+defaultconf(const struct environment *env)
 {
-	static char confbuf[PATH_MAX], homebuf[PATH_MAX];
-	static char hostbuf[HOST_NAME_MAX + 1];
+	static char buf[PATH_MAX];
+	int len, n;
+
+	len = sizeof(buf);
+	n = snprintf(buf, len, "%s/.mdsort.conf", env->home);
+	if (n == -1 || n >= len)
+		errx(1, "%s: buffer too small", __func__);
+	return buf;
+}
+
+static void
+readenv(struct environment *env)
+{
 	struct passwd *pw;
 	char *p;
-	size_t len;
-	int n;
 
-	if (gethostname(hostbuf, sizeof(hostbuf)) == -1)
+	if (gethostname(env->hostname, sizeof(env->hostname)) == -1)
 		err(1, "gethostname");
-	else if ((p = strchr(hostbuf, '.')) != NULL)
+	else if ((p = strchr(env->hostname, '.')) != NULL)
 		*p = '\0';
-	hostname = hostbuf;
 
 	if ((p = getenv("HOME")) == NULL || *p == '\0') {
 		log_debug("%s: HOME: unset or empty\n", __func__);
 		pw = getpwuid(getuid());
 		if (pw != NULL)
 			p = pw->pw_dir;
+		else
+			p = NULL;
 	}
 	if (p == NULL)
 		errx(1, "%s: cannot find home directory", __func__);
-	if ((len = strlen(p) + 1) > sizeof(homebuf))
+	if (strlcpy(env->home, p, sizeof(env->home)) >= sizeof(env->home))
 		errx(1, "%s: buffer too small", __func__);
-	memcpy(homebuf, p, len);
-	home = homebuf;
-
-	if (confpath != NULL)
-		return;
-	n = snprintf(confbuf, PATH_MAX, "%s/.mdsort.conf", home);
-	if (n == -1 || n >= PATH_MAX)
-		errx(1, "%s: buffer too small", __func__);
-	confpath = confbuf;
 }

extern.h
@@ -5,6 +5,7 @@
 #endif
 
 /* Forward declarations. */
+struct environment;
 struct expr;
 struct message;
 struct string_list;
@@ -48,7 +49,7 @@ int maildir_walk(struct maildir *md, char *buf);
  * Returns zero on success, non-zero otherwise.
  */
 int maildir_move(const struct maildir *src, const struct maildir *dst,
-    struct message *msg);
+    struct message *msg, const struct environment *env);
 
 /*
  * Parse the message located at path.
@@ -168,12 +169,18 @@ struct config {
 
 TAILQ_HEAD(config_list, config);
 
+struct environment {
+	char home[PATH_MAX];
+	char hostname[HOST_NAME_MAX + 1];
+};
+
 /*
  * Parses the configuration located at path and returns a config list on
  * success.
  * Otherwise, NULL is returned.
  */
-struct config_list *parse_config(const char *path);
+struct config_list *parse_config(const char *path,
+    const struct environment *env);
 
 /*
  * Join root, dirname and filename into a path written to buf which must be at
@@ -198,5 +205,4 @@ void log_debug(const char *fmt, ...)
 void log_info(const char *fmt, ...)
 	__attribute__((__format__ (printf, 1, 2)));
 
-extern const char *home, *hostname;
 extern int verbose;

maildir.c
@@ -33,7 +33,7 @@ struct maildir {
 static int maildir_create(struct maildir *);
 static int maildir_next(struct maildir *);
 static const char *maildir_genname(const struct maildir *,
-    const struct maildir *, struct message *);
+    const struct maildir *, struct message *, const struct environment *);
 static int maildir_read(struct maildir *, char *);
 static const char *maildir_root(struct maildir *);
 
@@ -114,7 +114,7 @@ maildir_walk(struct maildir *md, char *buf)
 
 int
 maildir_move(const struct maildir *src, const struct maildir *dst,
-    struct message *msg)
+    struct message *msg, const struct environment *env)
 {
 	char buf[NAME_MAX];
 	struct timespec times[2] = {
@@ -140,7 +140,7 @@ maildir_move(const struct maildir *src, const struct maildir *dst,
 		warn("fstatat");
 	}
 
-	dstname = maildir_genname(src, dst, msg);
+	dstname = maildir_genname(src, dst, msg, env);
 	dstfd = dirfd(dst->dir);
 
 	if (renameat(srcfd, srcname, dstfd, dstname) == -1) {
@@ -203,7 +203,7 @@ maildir_next(struct maildir *md)
 
 static const char *
 maildir_genname(const struct maildir *src, const struct maildir *dst,
-    struct message *msg)
+    struct message *msg, const struct environment *env)
 {
 	static char fname[NAME_MAX];
 	const char *flags;
@@ -220,7 +220,7 @@ maildir_genname(const struct maildir *src, const struct maildir *dst,
 		count++;
 		ts = time(NULL);
 		n = snprintf(fname, NAME_MAX, "%lld.%d_%d.%s%s",
-		    ts, getpid(), count, hostname, flags);
+		    ts, getpid(), count, env->hostname, flags);
 		if (n == -1 || n >= NAME_MAX)
 			errx(1, "%s: buffer too small", __func__);
 		fd = openat(dirfd(dst->dir), fname, O_WRONLY | O_CREAT | O_EXCL,

parse.y
@@ -20,6 +20,7 @@ static int yypeek(int);
 static void yyungetc(int);
 
 static struct config_list config = TAILQ_HEAD_INITIALIZER(config);
+static const struct environment *env;
 static FILE *fh;
 static const char *confpath;
 static int lineno, lineno_save, parse_errors;
@@ -233,8 +234,9 @@ nl		: '\n' optnl
 %%
 
 struct config_list *
-parse_config(const char *path)
+parse_config(const char *path, const struct environment *arg)
 {
+	env = arg;
 	fh = fopen(path, "r");
 	if (fh == NULL) {
 		warn("%s", path);
@@ -245,6 +247,7 @@ parse_config(const char *path)
 	lineno = 1;
 	yyparse();
 	fclose(fh);
+	env = NULL;
 	if (parse_errors > 0)
 		return NULL;
 	return &config;
@@ -386,27 +389,21 @@ again:
 static char *
 expandtilde(char *str)
 {
-	size_t hlen, slen;
+	char *buf;
+	int len, n;
 
 	if (*str != '~')
 		return str;
 
-	hlen = strlen(home);
-	slen = strlen(str);
-	if (hlen + slen - 1 >= PATH_MAX - 1) {
-		yyerror("path too long");
-		return NULL;
-	}
-	str = realloc(str, hlen + slen + 1);
-	if (str == NULL)
+	len = PATH_MAX;
+	buf = malloc(len);
+	if (buf == NULL)
 		err(1, NULL);
-	/*
-	 * Do not copy leading tilde, slen will therefore guarantee
-	 * NUL-termination.
-	 */
-	memmove(str + hlen, str + 1, slen);
-	memcpy(str, home, hlen);
-	return str;
+	n = snprintf(buf, len, "%s%s", env->home, str + 1);
+	if (n == -1 || n >= len)
+		yyerror("path too long");
+	free(str);
+	return buf;
 }
 
 static int

commit d543f560ac4a934353d4fbcf34136ee71cd7f186

Initialize confpath
Regression introduced in commit abc73d8 ("Reduce usage of globals by introducing
struct environment").

While here, sort local variables by size.

mdsort.c
@@ -20,12 +20,13 @@ int
 main(int argc, char *argv[])
 {
 	char path[PATH_MAX];
+	struct environment env;
 	struct config_list *config;
 	struct config *conf;
-	struct environment env;
 	struct maildir *dst, *md;
 	struct message *msg;
-	const char *confpath, *dstpath;
+	const char *dstpath;
+	const char *confpath = NULL;
 	int c;
 	int dflag = 0;
 	int mdflags = MAILDIR_CREATE;