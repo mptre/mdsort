mptre/mdsort

commit fcac5e646953c956c98fcdfe372d05db3897b204

add reject action
Used to reject a message read from stdin.

Makefile
@@ -64,6 +64,7 @@ DISTFILES+=	tests/match-header.sh
 DISTFILES+=	tests/match-logical.sh
 DISTFILES+=	tests/match-new.sh
 DISTFILES+=	tests/match-old.sh
+DISTFILES+=	tests/match-reject.sh
 DISTFILES+=	tests/stdin.sh
 DISTFILES+=	tests/t.sh
 DISTFILES+=	tests/util.sh

tests/Makefile
@@ -2,6 +2,7 @@ TESTS+=	action-break.sh
 TESTS+=	action-discard.sh
 TESTS+=	action-flag.sh
 TESTS+=	action-label.sh
+TESTS+=	action-reject.sh
 TESTS+=	basic.sh
 TESTS+=	conf.sh
 TESTS+=	dry.sh

expr.c
@@ -30,6 +30,7 @@ static int expr_eval_neg(EXPR_EVAL_ARGS);
 static int expr_eval_new(EXPR_EVAL_ARGS);
 static int expr_eval_old(EXPR_EVAL_ARGS);
 static int expr_eval_or(EXPR_EVAL_ARGS);
+static int expr_eval_reject(EXPR_EVAL_ARGS);
 
 static void expr_inspect_date(const struct expr *, FILE *,
     const struct environment *);
@@ -73,6 +74,7 @@ expr_alloc(enum expr_type type, int lno, struct expr *lhs, struct expr *rhs)
 	case EXPR_TYPE_DISCARD:
 	case EXPR_TYPE_BREAK:
 	case EXPR_TYPE_LABEL:
+	case EXPR_TYPE_REJECT:
 		ex->match = calloc(1, sizeof(*ex->match));
 		if (ex->match == NULL)
 			err(1, NULL);
@@ -217,6 +219,8 @@ expr_eval(struct expr *ex, struct match_list *ml, struct message *msg,
 		return expr_eval_break(ex, ml, msg, env);
 	case EXPR_TYPE_LABEL:
 		return expr_eval_label(ex, ml, msg, env);
+	case EXPR_TYPE_REJECT:
+		return expr_eval_reject(ex, ml, msg, env);
 	}
 
 	return 1;
@@ -269,6 +273,7 @@ expr_count_actions(const struct expr *ex)
 	case EXPR_TYPE_DISCARD:
 	case EXPR_TYPE_BREAK:
 	case EXPR_TYPE_LABEL:
+	case EXPR_TYPE_REJECT:
 		n = 1;
 		break;
 	}
@@ -321,6 +326,7 @@ expr_inspect(const struct expr *ex, FILE *fh, const struct environment *env)
 	case EXPR_TYPE_DISCARD:
 	case EXPR_TYPE_BREAK:
 	case EXPR_TYPE_LABEL:
+	case EXPR_TYPE_REJECT:
 		break;
 	}
 }
@@ -627,6 +633,21 @@ expr_eval_or(struct expr *ex, struct match_list *ml, struct message *msg,
 	if (expr_eval(ex->lhs, ml, msg, env) == 0)
 		return 0; /* match, short-circuit */
 	return expr_eval(ex->rhs, ml, msg, env);
+}
+
+static int
+expr_eval_reject(struct expr *ex, struct match_list *ml,
+    struct message *UNUSED(msg), const struct environment *UNUSED(env))
+{
+	size_t len;
+
+	matches_append(ml, ex->match);
+
+	len = sizeof(ml->ml_path);
+	if (strlcpy(ml->ml_path, "<reject>", len) >= len)
+		errc(1, ENAMETOOLONG, "%s", __func__);
+
+	return 0;
 }
 
 static void

extern.h
@@ -131,6 +131,7 @@ enum expr_type {
 	EXPR_TYPE_DISCARD,
 	EXPR_TYPE_BREAK,
 	EXPR_TYPE_LABEL,
+	EXPR_TYPE_REJECT,
 };
 
 enum expr_cmp {
@@ -218,7 +219,7 @@ void matches_clear(struct match_list *ml);
 int matches_interpolate(struct match_list *ml, struct message *msg);
 
 int matches_exec(const struct match_list *ml, struct maildir *md,
-    struct message *msg, const struct environment *env);
+    struct message *msg, int *reject, const struct environment *env);
 
 void matches_inspect(const struct match_list *ml, FILE *fh,
     const struct environment *env);

match.c
@@ -56,8 +56,9 @@ matches_interpolate(struct match_list *ml, struct message *msg)
 	char *label, *path;
 	size_t len;
 
-	/* Discard is mutually exclusive with all other actions. */
-	if (matches_find(ml, EXPR_TYPE_DISCARD) != NULL)
+	/* Actions lacking support for interpolation. */
+	if (matches_find(ml, EXPR_TYPE_DISCARD) != NULL ||
+	    matches_find(ml, EXPR_TYPE_REJECT) != NULL)
 		return 0;
 
 	if (ml->ml_maildir[0] == '\0') {
@@ -96,7 +97,7 @@ matches_interpolate(struct match_list *ml, struct message *msg)
 
 int
 matches_exec(const struct match_list *ml, struct maildir *src,
-    struct message *msg, const struct environment *env)
+    struct message *msg, int *reject, const struct environment *env)
 {
 	char path[NAME_MAX];
 	struct maildir *dst;
@@ -136,6 +137,9 @@ matches_exec(const struct match_list *ml, struct maildir *src,
 				error = 1;
 			else
 				msg->path = path;
+			break;
+		case EXPR_TYPE_REJECT:
+			*reject = 1;
 			break;
 		default:
 			break;

mdsort.c
@@ -17,6 +17,14 @@
  */
 #define EX_TEMPFAIL	75
 
+/*
+ * When reading messages from stdin and a reject rule is matched, always exit
+ * non-zero indicating permanent failure.
+ * The actual number is quite arbitrary but something that at least OpenSMTPD
+ * will interpret as a permanent failure during mda delivery.
+ */
+#define EX_PERMFAIL	1
+
 static const char *defaultconf(const struct environment *);
 static void readenv(struct environment *);
 static __dead void usage(void);
@@ -34,6 +42,7 @@ main(int argc, char *argv[])
 	unsigned int mdflags = MAILDIR_WALK;
 	int c;
 	int error = 0;
+	int reject = 0;
 	int verbose = 0;
 
 	if (pledge("stdio rpath wpath cpath fattr getpw", NULL) == -1)
@@ -135,7 +144,7 @@ main(int argc, char *argv[])
 				continue;
 			}
 
-			if (matches_exec(&matches, md, msg, &env))
+			if (matches_exec(&matches, md, msg, &reject, &env))
 				error = 1;
 			message_free(msg);
 		}
@@ -145,8 +154,12 @@ main(int argc, char *argv[])
 out:
 	config_free(config);
 
-	if (error && (env.ev_options & OPTION_STDIN))
-		return EX_TEMPFAIL;
+	if (env.ev_options & OPTION_STDIN) {
+		if (error)
+			return EX_TEMPFAIL;
+		if (reject)
+			return EX_PERMFAIL;
+	}
 
 	return error;
 }

mdsort.conf.5
@@ -203,6 +203,20 @@ Only one
 .Ic move
 action can be defined per rule.
 .El
+.Pp
+In addition,
+.Ic stdin
+also supports the following actions:
+.Bl -tag -width Ds
+.It Ic reject
+Reject the message by causing
+.Xr mdsort 1
+to exit non-zero.
+.Pp
+Only one
+.Ic reject
+action can be defined per rule.
+.El
 .It Ic match Ar condition No { Ar rule ... No }
 The nested block of rules
 is only evaluated if

parse.y
@@ -58,6 +58,7 @@ static int lineno, lineno_save, parse_errors;
 %token NEW
 %token OLD
 %token PATTERN
+%token REJECT
 %token SCALAR
 %token STDIN
 %token STRING
@@ -86,6 +87,10 @@ maildir		: maildir_path exprblock {
 			/* Favor more specific error messages. */
 			if (parse_errors == 0 && expr_count_actions($2) == 0)
 				yyerror("empty match block");
+
+			if ($1 != NULL &&
+			    expr_count($2, EXPR_TYPE_REJECT) > 0)
+				yyerror("reject cannot be used outside stdin");
 
 			conf = malloc(sizeof(*conf));
 			if (conf == NULL)
@@ -245,6 +250,9 @@ expraction	: BREAK {
 			$$ = expr_alloc(EXPR_TYPE_LABEL, lineno, NULL, NULL);
 			expr_set_strings($$, $2);
 		}
+		| REJECT {
+			$$ = expr_alloc(EXPR_TYPE_REJECT, lineno, NULL, NULL);
+		}
 		;
 
 
@@ -398,6 +406,7 @@ yylex(void)
 		{ "new",	NEW },
 		{ "old",	OLD },
 		{ "or",		OR },
+		{ "reject",	REJECT },
 		{ "stdin",	STDIN },
 
 		{ NULL,		0 },
@@ -602,6 +611,8 @@ expr_validate(const struct expr *ex)
 		yyerror("break cannot be combined with another action");
 	if (expr_count(ex, EXPR_TYPE_DISCARD) > 0)
 		yyerror("discard cannot be combined with another action");
+	if (expr_count(ex, EXPR_TYPE_REJECT) > 0)
+		yyerror("reject cannot be combined with another action");
 }
 
 static int

commit 5c23819b182a3bb766d4f37da3840185726b9581

fix incorrect path in DISTFILES
Typo introduced in commit fcac5e6 (add reject action).

Makefile
@@ -53,6 +53,7 @@ DISTFILES+=	tests/action-discard.sh
 DISTFILES+=	tests/action-flag.sh
 DISTFILES+=	tests/action-label.sh
 DISTFILES+=	tests/action-pass.sh
+DISTFILES+=	tests/action-reject.sh
 DISTFILES+=	tests/basic.sh
 DISTFILES+=	tests/conf.sh
 DISTFILES+=	tests/dry.sh
@@ -65,7 +66,6 @@ DISTFILES+=	tests/match-header.sh
 DISTFILES+=	tests/match-logical.sh
 DISTFILES+=	tests/match-new.sh
 DISTFILES+=	tests/match-old.sh
-DISTFILES+=	tests/match-reject.sh
 DISTFILES+=	tests/stdin.sh
 DISTFILES+=	tests/t.sh
 DISTFILES+=	tests/util.sh