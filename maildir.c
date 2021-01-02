mptre/mdsort

commit b8acf458bec5ea1e7050c9ccbeea09fb3bc3898c

add readdir unknown file type fault

maildir.c
@@ -442,6 +442,7 @@ static int
 maildir_read(struct maildir *md, struct maildir_entry *me)
 {
 	const struct dirent *ent;
+	unsigned int type;
 
 	if (FAULT("maildir_read", md->md_path))
 		return -1;
@@ -461,7 +462,11 @@ maildir_read(struct maildir *md, struct maildir_entry *me)
 			return 0;
 		}
 
-		switch (ent->d_type) {
+		type = ent->d_type;
+		if (FAULT("readdir_type", md->md_path, ent->d_name))
+			type = DT_UNKNOWN;
+
+		switch (type) {
 		case DT_UNKNOWN:
 			/*
 			 * Some filesystems like XFS does not return the file
@@ -477,7 +482,7 @@ maildir_read(struct maildir *md, struct maildir_entry *me)
 		default:
 unknown:
 			log_debug("%s: %s/%s: unknown file type %d\n",
-			    __func__, md->md_path, ent->d_name, ent->d_type);
+			    __func__, md->md_path, ent->d_name, type);
 			continue;
 		}
 

fault.c
@@ -11,8 +11,10 @@
 
 #ifdef DIAGNOSTIC
 
+#define ARGS_MAX	1024
+
 struct fault {
-	char fu_args[256];
+	char fu_args[ARGS_MAX];
 	char fu_name[32];
 	int fu_errno;
 	unsigned int fu_hits;
@@ -32,19 +34,30 @@ static int cold = 1;
 int
 fault(const char *name, ...)
 {
+	char buf[ARGS_MAX];
 	va_list ap;
+	ssize_t bufsiz = sizeof(buf);
 	int match = 0;
+	int n;
 
 	fault_init();
 
+#define STRARG	va_arg(ap, const char *)
 	va_start(ap, name);
-	if (strcmp(name, "maildir_read") == 0)
-		match = fault_match("maildir_read", va_arg(ap, const char *));
-	else if (strcmp(name, "maildir_unlink") == 0)
-		match = fault_match("maildir_unlink", va_arg(ap, const char *));
-	else
+	if (strcmp(name, "maildir_read") == 0) {
+		match = fault_match("maildir_read", STRARG);
+	} else if (strcmp(name, "maildir_unlink") == 0) {
+		match = fault_match("maildir_unlink", STRARG);
+	} else if (strcmp(name, "readdir_type") == 0) {
+		n = snprintf(buf, bufsiz, "%s, %s", STRARG, STRARG);
+		if (n < 0 || n >= bufsiz)
+			errc(1, ENAMETOOLONG, "%s", __func__);
+		match = fault_match("readdir_type", buf);
+	} else {
 		errx(1, "%s: %s: unknown fault", __func__, name);
+	}
 	va_end(ap);
+#undef STRARG
 
 	if (match)
 		warnx("%s: %s", __func__, name);

tests/basic.sh
@@ -285,7 +285,7 @@ if testcase "pattern delimiter"; then
 	refute_empty "dst/new"
 fi
 
-if testcase -t fault "maildir walk failure"; then
+if testcase -t fault "readdir failure"; then
 	mkmd "src"
 	mkmsg "src/new"
 	cat <<-EOF >"$CONF"
@@ -296,4 +296,19 @@ if testcase -t fault "maildir walk failure"; then
 	mdsort -e -f "name=maildir_read,errno=EINVAL" - <<-EOF
 	mdsort: fault: maildir_read
 	EOF
+fi
+
+if testcase -t fault "readdir unknown file type"; then
+	mkmd "src" "dst"
+	mkmsg "src/new"
+	cat <<-EOF >"$CONF"
+	maildir "src" {
+		match all move "dst"
+	}
+	EOF
+	mdsort -f "name=readdir_type" - <<-EOF
+	mdsort: fault: readdir_type
+	EOF
+	assert_empty "src/new"
+	refute_empty "dst/new"
 fi

commit 3edd9c1076cee897f9b9aa95cac1dd83f39139e9

correct signedness in log_debug()
Regression introduced in commit b8acf45 ("add readdir unknown file type
fault").

maildir.c
@@ -481,7 +481,7 @@ maildir_read(struct maildir *md, struct maildir_entry *me)
 			break;
 		default:
 unknown:
-			log_debug("%s: %s/%s: unknown file type %d\n",
+			log_debug("%s: %s/%s: unknown file type %u\n",
 			    __func__, md->md_path, ent->d_name, type);
 			continue;
 		}