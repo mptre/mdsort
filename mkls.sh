ca638ee7e6bf43dcf143e1a5a4f8597a279c0c20
Anton Lindqvist <anton@basename.se>
2021-12-01 20:27:29 +0100

    add mkls wrapper

---
+++ mkls.sh
@@ -0,0 +1,21 @@
+set -e
+
+mkls "$@" \
+SRCS \
+	*.c \
+	parse.c \
+	-- \
+KNFMT \
+	!(compat-*).c !(config|compat-queue).h \
+	compat-?(arc4random|pledge|utimensat).c \
+	-- \
+DISTFILES \
+	*.1 *.5 *.c !(config).h *.md \
+	GNUmakefile LICENSE Makefile configure parse.y \
+	tests/*.sh \
+	tests/GNUmakefile tests/Makefile
+
+cd tests
+mkls "$@" \
+TESTS \
+	!(t|util).sh
