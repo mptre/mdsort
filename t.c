191562ee0e6c29b93c7bf41d17c331bde19d83af
Anton Lindqvist <anton@basename.se>
2023-02-05 21:12:19 +0100

    add base64 unit tests

--- t.c
+++ t.c
@@ -1,13 +1,17 @@
 #include "config.h"
 
-// XXX
-#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #include "decode.h"
+
+#define test_base64_decode(a, b)					\
+	error |= test_base64_decode0((a), (b), "base64_decode", __LINE__);\
+	if (xflag && error) goto out;
+static int	test_base64_decode0(const char *, const char *, const char *,
+    int);
 
 #define test_rfc2047_decode(a, b)					\
 	error |= test_rfc2047_decode0((a), (b), "rfc2047_decode", __LINE__);\
@@ -34,8 +38,23 @@ main(int argc, char *argv[])
 		}
 	}
 
+	test_base64_decode("", "");
+	test_base64_decode("Zg==", "f");
+	test_base64_decode("Zm8=", "fo");
+	test_base64_decode("Zm9v", "foo");
+	test_base64_decode("Zm9vYg==", "foob");
+	test_base64_decode("Zm9vYmE=", "fooba");
+	test_base64_decode("Zm9vYmFy", "foobar");
+	test_base64_decode(
+	    "YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFh"
+	    "YWFhCmJiYmJiYmJiYmJiYmJiYmJiYmJiYmJiYmJiYmJiYmJiYmJiYmJiYmJiYmJiYmJiYmJiYmJi"
+	    "YmJiYmJiYgo=",
+	    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n"
+	    "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb\n");
+
 	test_rfc2047_decode("", "");
 	test_rfc2047_decode("a", "a");
+	test_rfc2047_decode("=?UTF-8?B?Zm9vYmFy?=", "foobar");
 	test_rfc2047_decode("=?UTF-8?Q?a?=", "a");
 	test_rfc2047_decode("(=?UTF-8?Q?a?=)", "(a)");
 	test_rfc2047_decode("(=?UTF-8?Q?a?= b)", "(a b)");
@@ -63,6 +82,22 @@ test_rfc2047_decode0(const char *str, const char *exp, const char *fun, int lno)
 	int error = 0;
 
 	act = rfc2047_decode(str);
+	if (strcmp(exp, act) != 0) {
+		fprintf(stderr, "%s:%d:\n\texp %s\n\tgot %s\n",
+		    fun, lno, exp, act);
+		error = 1;
+	}
+	free(act);
+	return error;
+}
+
+static int
+test_base64_decode0(const char *str, const char *exp, const char *fun, int lno)
+{
+	char *act;
+	int error = 0;
+
+	act = base64_decode(str);
 	if (strcmp(exp, act) != 0) {
 		fprintf(stderr, "%s:%d:\n\texp %s\n\tgot %s\n",
 		    fun, lno, exp, act);
