14f7f647e8791dfb4ca81ddd0b9e95e4ec8a1feb
Anton Lindqvist <anton@basename.se>
2023-07-03 07:56:44 +0200

    sync mkls.sh

--- mkls.sh
+++ mkls.sh
@@ -1,25 +1,11 @@
-export LC_ALL=C
-
-mkls "$@" \
-SRCS \
-	!(fuzz-*|mdsort|t).c \
-	parse.c \
-	$(find libks -type f -name '*.c' -exec basename {} \;) \
-	-- \
-KNFMT \
-	!(compat-*|parse).c !(config|compat-queue).h \
-	compat-?(arc4random|pledge|utimensat).c \
-	-- \
-CLANGTIDY \
-	!(compat-*|parse).c !(config|compat-queue).h \
-	compat-?(arc4random|pledge|utimensat).c \
-	-- \
-CPPCHECK \
-	!(compat-*|parse).c compat-?(arc4random|pledge|utimensat).c \
-	-- \
-SHLINT \
-	configure \
-	-- \
+mkls -s "$@" -- \
+SRCS		!(fuzz-*|mdsort|t).c parse.c $(cd libks && ls *.c) -- \
+KNFMT		!(compat-*|parse).c !(config|compat-queue).h \
+		compat-?(arc4random|pledge|utimensat).c -- \
+CLANGTIDY	!(compat-*|parse).c !(config|compat-queue).h \
+		compat-?(arc4random|pledge|utimensat).c -- \
+CPPCHECK	!(compat-*|parse).c compat-?(arc4random|pledge|utimensat).c -- \
+SHLINT		configure -- \
 DISTFILES \
 	*.1 *.5 !(parse).c !(config).h libks/*.[ch] *.md \
 	GNUmakefile LICENSE Makefile configure parse.y \
@@ -27,6 +13,5 @@ DISTFILES \
 	tests/GNUmakefile tests/Makefile
 
 cd tests
-mkls "$@" \
-TESTS \
-	!(t|util).sh
+mkls -s "$@" -- \
+TESTS	!(t|util).sh
